b6149570d024ede4aec39ee149d510010d9373ae
herehereiam <christopher.lh.55@gmail.com>
2023-06-03 11:27:31 -0700

    Ball has randomized velocity for click-in-place; radius, bounce, gravity sliders and gravity selector buttons added; only radius and bounce sliders implemented

--- ball.js
+++ ball.js
@@ -1,34 +1,46 @@
+let power = 15;
+
 class Ball
 {
-	constructor(frameLength, screen, radius, mouseXPos, mouseYPos)
+	constructor(frameLength, screen, radius, cor, xAcc, yAcc, mouseXPos, mouseYPos)
 	{
 		this.element = document.createElement('span');
 		this.frameLength = frameLength; // in seconds
 		this.radius = radius;
-		this.xAcc = 0; // in px per second
-		this.yAcc = 5000;
+		this.cor = cor // bounciness
+		this.xAcc = xAcc; // in px per second
+		this.yAcc = yAcc;
 		this.xVel = 0;
 		this.yVel = 0;
 		this.xPos = mouseXPos - this.radius;
 		this.yPos = mouseYPos - this.radius;
-		this.cor = 0.8 // bounciness
 
+		// generate circle
 		this.element.className = 'circle';
 		this.element.style.width = 2*this.radius + 'px';
 		this.element.style.height = 2*this.radius + 'px';
 		this.element.style.position = 'absolute';
 		this.element.style.left = this.xPos + 'px';
 		this.element.style.top = this.yPos + 'px';
-
 		screen.appendChild(this.element);
+
 		document.addEventListener('mouseup', this.release.bind(this), {once: true});
 	}
 
 	release(event)
 	{
-		let power = 15;
-		this.xVel = power * ((this.xPos+this.radius) - event.clientX);
-		this.yVel = power * ((this.yPos+this.radius) - event.clientY);
+		let xDiff = (this.xPos+this.radius) - event.clientX;
+		let yDiff = (this.yPos+this.radius) - event.clientY;
+
+		// randomize initial velocity if user clicked in place (e.g. on a touchscreen)
+		if (xDiff == 0 && yDiff == 0)
+		{
+			xDiff = this.xPos - (Math.random() * screen.offsetWidth);
+			yDiff = this.yPos - (Math.random() * screen.offsetHeight);
+		}
+
+		this.xVel = power * xDiff;
+		this.yVel = power * yDiff;
 		setInterval(this.redraw.bind(this), this.frameLength*1000);
 	}
 
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const fps = 60;
 let screen;
 let radius = 50;
+let cor = 0.8;
+let xAcc = 0;
+let yAcc = 5000;
 
 function generate(event)
 {
-	ball = new Ball(1/fps, screen, radius, event.clientX, event.clientY);
+	radius = document.querySelector('#radiusSlider').value * 0.5 * Math.min(screen.offsetWidth, screen.offsetHeight);
+	cor = document.querySelector('#corSlider').value;
+	ball = new Ball(1/fps, screen, radius, cor, xAcc, yAcc, event.clientX, event.clientY);
 }
 
 document.addEventListener('DOMContentLoaded', function() 
 {
-	screen = document.querySelector('div');
-	document.addEventListener('mousedown', generate);
+	screen = document.querySelector('#screen');
+	screen.addEventListener('mousedown', generate);
 });
 
-// TODO make radius, bounce sliders; gravity selector; colors
+// TODO make radius, bounce sliders; gravity slider, selector; colors
